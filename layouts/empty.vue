d0bbcf7c56c9df4236601b39d57e05acf4a16b01
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-05-29 14:42:13 -0500

    Tailwind & Flowbite setup

---
+++ layouts/empty.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="_empty">
+    <slot/>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <p>Login page content goes here.</p>
+  </div>
+</template>
