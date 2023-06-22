0baad121fb104ff6132f2ace2ff817c354134acf
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-06-22 12:01:21 -0500

    Partial cart composable

--- components/card/ProductCard.vue
+++ components/card/ProductCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // composables
 const config = useRuntimeConfig()
+const cart = useCart()
 
 // props
 const props = defineProps({
@@ -35,6 +36,11 @@ const sizes = [
 ]
 
 const isShow = ref(false)
+
+// methods
+const onAdd = (item: any) => {
+  cart.add(item)
+}
 
 
 </script>
@@ -100,11 +106,13 @@ const isShow = ref(false)
           <path stroke-linecap="round" stroke-linejoin="round"
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"></path>
         </svg>
-        <svg fill="none" width="40" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
-             xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"></path>
-        </svg>
+        <button @click="onAdd(product)">
+          <svg fill="none" width="40" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+               xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"></path>
+          </svg>
+        </button>
       </div>
     </article>
   </article>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    runtimeConfig: {
-        public: {
-            baseURL: process.env.BASE_URL || 'http://localhost:3000',
-        }
-    },
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/google-fonts'
-    ],
-    googleFonts: {
-        families: {
-            Poppins: {
-                wght: [100, 200, 300, 400, 500, 600, 700],
-            },
-            Inter: {
-                wght: [100, 200, 300, 400, 500, 600, 700],
-            }
-        }
-    },
-    tailwindcss: {
-        cssPath: '~/assets/css/tailwind.css',
-        configPath: 'tailwind.config',
-    },
-    css: [
-        '~/assets/css/main.css',
-    ],
+  runtimeConfig: {
+      public: {
+          baseURL: process.env.BASE_URL || 'http://localhost:3000',
+      }
+  },
+
+  modules: [
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/google-fonts',
+      '@pinia/nuxt',
+  ],
+
+  googleFonts: {
+      families: {
+          Poppins: {
+              wght: [100, 200, 300, 400, 500, 600, 700],
+          },
+          Inter: {
+              wght: [100, 200, 300, 400, 500, 600, 700],
+          }
+      }
+  },
+
+  tailwindcss: {
+      cssPath: '~/assets/css/tailwind.css',
+      configPath: 'tailwind.config',
+  },
+
+  css: [
+      '~/assets/css/main.css',
+  ],
+
+  devtools: {
+    enabled: true,
+  },
 })
