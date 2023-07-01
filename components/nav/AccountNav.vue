8bf9cbe49573917dc57c17314f0b2a6db7357e20
Juan Esteban <122553739+DrNewtonsoft@users.noreply.github.com>
2023-07-01 13:33:13 +0000

    Myaccount page & Partial Table View

---
+++ components/nav/AccountNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="w-full h-auto">
+        <nav class="flex flex-col">
+            <ul class="fonts-poppins text-lg font-bold">
+                <li class="border-b border-gray-300">
+                    <button class="focus:text-primary-600">Pedidos</button>
+                </li>
+                <li class="border-b border-gray-300">
+                    <button class="focus:text-primary-600">Direcciones</button>
+                </li>
+                <li class="border-b border-gray-300">
+                    <button class="focus:text-primary-600">Detalles de la cuenta</button>
+                </li>
+                <li class="border-b border-gray-300">
+                    <button class="focus:text-primary-600">Salir</button>
+                </li>
+            </ul>
+        </nav>
+    </section>
+</template>
---
+++ pages/myaccount.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="flex flex-col gap-6">
+        <SectionBannerAccountSection/>
+        <section class="grid grid-cols-4 md:grid-cols-11 lg:grid-cols-12 gap-3 sm:gap-4">
+            <NavAccountNav class="grid-span-2 col-start-2 col-end-4" />
+            <SectionAccountViewsSection class="grid-span-8 col-start-3 col-end-12"/>
+        </section>
+    </section>
+</template>
+<script setup>
+</script>
