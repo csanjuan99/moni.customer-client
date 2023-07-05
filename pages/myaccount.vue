cc82171550a127c669a02ff4709d55f58ecfd3d9
Bryan Vanegas <brayanxdvanegas@gmail.com>
2023-07-05 11:52:40 -0500

    Arreglando un par de estilos y llamados

--- pages/myaccount.vue
+++ pages/myaccount.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="flex flex-col gap-6">
         <SectionBannerAccountSection/>
-        <section class="grid grid-cols-4 md:grid-cols-11 lg:grid-cols-12 gap-3 sm:gap-4">
-            <NavAccountNav class="grid-span-2 col-start-2 col-end-4" />
-            <SectionAccountViewsSection class="grid-span-8 col-start-3 col-end-12"/>
+        <section class="grid grid-cols-4 md:grid-cols-12 gap-3 md:gap-4">
+            <NavAccountNav class="col-span-2 col-start-2" />
+            <SectionAccountViewsSection class="col-span-8"/>
         </section>
     </section>
 </template>
