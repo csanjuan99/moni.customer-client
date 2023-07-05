06d20e2b1cee9d18cf2e16643bba5570128f0adf
johan <johan352@gmail.com>
2023-07-05 08:54:55 -0500

    VDC0-43 Terminado

--- components/section/AccountDetailSection.vue
+++ components/section/AccountDetailSection.vue
@@ -1,6 +1,6 @@
 <template>
      <section class="p-8">
-        <div class="grid grid-cols-4 md:grid-cols-12 gap-4">
+        <div class="grid grid-cols-4 md:grid-cols-12 gap-4 m-10">
             <div class="col-span-4 md:col-span-8 md:col-start-2">
                 <p class="xs:text-sm md:text-xl md:mt-8 font-bold font-poppins text-gray-900 mt-3 mx-3 ">General information</p>
 
@@ -24,7 +24,7 @@
                 </form>
 
                 <p class="xs:text-sm md:text-xl md:mt-8 font-bold font-poppins text-gray-900 mt-3 mx-3 ">Password information</p>
-                <form action="#" class=" text-sm grid grid-cols-1 gap-4 p-3 mx-auto text-gray-900 font-poppins font-normal max-w-screen-md sm:grid-cols-2">
+                <form action="#" class=" text-sm grid grid-cols-1 gap-4 p-3 mx-auto text-gray-900 font-poppins font-normal max-w-screen-md sm:grid-cols-2 mb-5">
                     <div>
                         <label for="first-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Primer Nombre</label>
                         <input type="text" id="first-name" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Thomas" required>
@@ -39,22 +39,21 @@
                     </div> 
                 </form>
 
-                <div class=" p-4 rounded-md">
-                    <h3 class="text-lg font-semibold mb-1">Password requirements:</h3>
-                    <p>Ensure that these requirements are met:</p>
-                    <ul class="list-none pl-6">
+                <div class=" p-4 rounded-md mb-10 mt-5">
+                    <h3 class="text-lg font-semibold mb-1 text-gray-900">Password requirements:</h3>
+                    <p class="text-gray-600">Ensure that these requirements are met:</p>
+                    <ul class="list-none pl-6 text-gray-600">
                         <li>At least 10 characters (and up to 100 characters)</li>
                         <li>At least one lowercase character</li>
                         <li>Inclusion of at least one special character, e.g., ! @ # ?</li>
                         <li>Inclusion of at least one special character, e.g., ! @ # ?</li>
                     </ul>
                 </div>
-
-                <button type="submit" class="py-3 mt-6 px-5 mx-3 md:col-span-2 xs:w-40 md:w-52 xs:my-auto flex items-center justify-center text-sm font-medium text-center text-white rounded-lg bg-black sm:w-fit hover:bg-gray-700 focus:bg-gray-00 focus:border-gray-500">
+                <button type="submit" class="py-3 px-5 mx-3 mt-10 md:col-span-2 xs:w-40 md:w-52 xs:my-auto flex items-center justify-center text-sm font-medium text-center text-white rounded-lg bg-black sm:w-fit hover:bg-gray-700 focus:bg-gray-00 focus:border-gray-500">
                     <span>Guardar dirección</span>
-                </button>
-            </div> 
-        </div>           
+                </button>   
+            </div>   
+        </div>       
     </section>
 </template>
 
